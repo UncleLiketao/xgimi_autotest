5b73499e185615045be2941c65c134214c006f54
win10 <414480664@qq.com>
2020-12-18 00:43:26 +0800

    [add] 新增shell自动化文件夹和组团升级稳定性脚本文件

---
+++ Shell_Automation/BundleApk_Stability.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "执行回复语出厂操作"
+echo "执行跳过开机向导操作"
+echo "捕获下载任务job"
+echo "等待升级完成，检查flag是否为1"
+echo "执行reboot"
+echo "开机后检查inui版本、flag值和status状态码"
+echo "开机后校验升级应用版本"
+echo "遍历启动应用并截图"
+循环执行以上所有操作
